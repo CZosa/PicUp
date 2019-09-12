04d85586b5c7801558c5970f4c80c948f4f2a927
Chris Zosa <zosachris@gmail.com>
2019-09-11 19:07:31 -0700

    App displays number of pictures retrieved from api

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 import '../styles/app.css';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term)
+  state = {
+    pictures: []
+  };
+
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
+      params: {
+        query: term
+      }
+    });
+    
+    this.setState({ pictures: response.data.results });
   }
 
   render() {
     return (
       <div className="container">
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <div className="img_amount">
+          There are {this.state.pictures.length} pictures available!
+        </div>
       </div>
     )
   }
